d3de4da7c6c0ab8a2e6540afd608e805c2cea6f7
benjasteck <benjasteckmetz@gmail.com>
2022-03-17 03:04:56 +0100

    added stage loaders

--- src/main/java/GUI/Controller/LogInController.java
+++ src/main/java/GUI/Controller/LogInController.java
@@ -1,11 +1,30 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogInController {
+
+
     public void logIn(ActionEvent actionEvent) {
+
+        /*
+        FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource("LogInView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 567, 326);
+        stage.setTitle("Sign In");
+        stage.setScene(scene);
+        stage.show();*/
     }
 
-    public void cancelLogin(ActionEvent actionEvent) {
+    public void cancelLogin(ActionEvent actionEvent, Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource("MainScreenController.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 567, 326);
+        stage.setTitle("Sign In");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/GUI/Controller/MainScreenController.java
+++ src/main/java/GUI/Controller/MainScreenController.java
@@ -1,8 +1,21 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainScreenController {
-    public void logIn(ActionEvent actionEvent) {
+
+    public void logIn(ActionEvent actionEvent, Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource("LogInView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 567, 326);
+        stage.setTitle("Sign In");
+        stage.setScene(scene);
+        stage.show();
     }
+
+
 }
